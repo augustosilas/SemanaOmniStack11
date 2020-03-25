e38dfe9a63da6bace715fd05ac76bf692fd1b766
Silas Augusto <augusto.fernandes775@gmail.com>
2020-03-25 15:52:52 -0300

    Conceito de estado e imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./Header";
 // JSX (JavaScript XML)
 // Componente é uma função que retorna HTML
 
 function App() {
-  return <Header title="Usando props">Semana OmniStack</Header>;
+  const [counter, setCounter] = useState(0);
+
+  function incremente() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={incremente}>Incrementar</button>
+    </div>
+  );
 }
 
 export default App;
